d575b6881f08f3759da43de9f402f2d0e34bb395
Michail Bredichin <michailbredichin@gmail.com>
2025-10-28 06:53:02 +0100

    started grid

--- snake/main.py
+++ snake/main.py
@@ -1,7 +1,7 @@
-from turtle import Turtle, Screen, color
+from turtle import Turtle, Screen, color, forward, goto
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
 CELL_WIDTH = 10
 CELL_HEIGHT = 10
 BG_COLOUR = 'black'
@@ -17,13 +17,36 @@ class Snake(Turtle):
 
 def main():
 
+
+
+
     screen = Screen()
     screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+    
+    # screen.tracer(0)
+
+    print(screen.canvheight, " ", screen.canvwidth)
     screen.bgcolor(BG_COLOUR) 
 
     snake = Snake()
-    snake.forward(100)
+    snake.forward(100)    
+    display_grid()
     screen.exitonclick()
+
+def display_grid():
+    startx =  -SCREEN_WIDTH/2 +20
+    starty =  SCREEN_HEIGHT/2
+    # endy = starty - SCREEN_HEIGHT + CELL_HEIGHT
+    
+    turtle = Turtle()
+    turtle.color('white')
+    turtle.penup()
+    turtle.goto(startx,starty)
+    turtle.right(90) 
+    turtle.pendown()
+    turtle.forward(SCREEN_HEIGHT)
+
+        
 
 if __name__ == "__main__":
     main()
