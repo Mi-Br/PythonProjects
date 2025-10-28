21a638d534d8ecf9e34681a9e2db5176ca27c893
Michail Bredichin <michailbredichin@gmail.com>
2025-10-28 09:12:44 +0100

    grid base done

--- snake/main.py
+++ snake/main.py
@@ -16,10 +16,6 @@ class Snake(Turtle):
         self.shape('square')
 
 def main():
-
-
-
-
     screen = Screen()
     screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
     
@@ -34,19 +30,36 @@ def main():
     screen.exitonclick()
 
 def display_grid():
-    startx =  -SCREEN_WIDTH/2 +20
+    startx =  -SCREEN_WIDTH/2
     starty =  SCREEN_HEIGHT/2
     # endy = starty - SCREEN_HEIGHT + CELL_HEIGHT
     
-    turtle = Turtle()
-    turtle.color('white')
-    turtle.penup()
-    turtle.goto(startx,starty)
-    turtle.right(90) 
-    turtle.pendown()
-    turtle.forward(SCREEN_HEIGHT)
+    gturtl = Turtle()
+    gturtl.color('white')
+    gturtl.right(90) 
+    gturtl.penup()
+    gturtl.goto(startx,starty)
+    gturtl.speed('fastest')
 
-        
+    for _ in range(int(SCREEN_WIDTH/CELL_WIDTH)):
+        gturtl.goto(startx,starty)
+        gturtl.pendown()
+        gturtl.forward(SCREEN_HEIGHT)
+        gturtl.penup()
+        startx = startx + CELL_WIDTH
+    
+    startx =  -SCREEN_WIDTH/2
+    starty =  SCREEN_HEIGHT/2
+
+    gturtl.penup()
+    gturtl.goto(startx,starty)
+    gturtl.left(90)
+    for _ in range(int(SCREEN_WIDTH/CELL_WIDTH)):
+        gturtl.pendown()
+        gturtl.forward(SCREEN_WIDTH)
+        gturtl.penup()
+        starty = starty - CELL_HEIGHT
+        gturtl.goto(startx,starty)
 
 if __name__ == "__main__":
     main()
